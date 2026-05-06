b75063c8b649d01000fddeef16217c99f0f9f735
aponiatowski <a.s.poniatowski@gmail.com>
2026-05-06 19:24:33 +1000

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.kt
@@ -0,0 +1,31 @@
+class Solution {
+
+    fun encode(strs: List<String>): String {
+        val sb = StringBuilder()
+
+        for (str in strs) {
+            sb.append(str.length).append('#').append(str)
+        }
+        return sb.toString()
+    }
+
+    fun decode(str: String): List<String> {
+        if (str.isEmpty()) return listOf()
+
+        val result = mutableListOf<String>()
+        var idx = 0
+        while (idx < str.length - 1) {
+            var wordLen = 0
+
+            while (str[idx].isDigit()) {
+                wordLen = wordLen * 10 + (str[idx] - '0')
+                idx++
+            }
+            idx++
+            val nextWord = str.substring(idx, idx + wordLen)
+            result.add(nextWord)
+            idx += wordLen
+        }
+        return result
+    }
+}
